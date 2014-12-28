142af509459f7c985720f653676c461025ed2d95
Hubert Olender <olender.hub@gmail.com>
2014-12-28 13:08:16 +0000

    appconnect1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sampleapphub.herokuapp.com'
+  host = 'appconnect.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
